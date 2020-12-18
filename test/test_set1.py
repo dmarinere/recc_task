a34f3c560f2cbfc7cdafaf3edca6fdbbf5484ba9
dmarinere <dmarinere@gmail.com>
2020-12-18 16:16:40 +0100

    committed

---
+++ test/test_set1.py
@@ -0,0 +1,14 @@
+import unittest
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_equal, assert_string_equal
+from numpy.testing import assert_allclose, assert_raises, assert_raises_regex
+from app import 
+
+
+
+def test_basic_test():
+    out = {}
+    assert_equal(isinstance(out, dict), True)
+    
+
+def test_
